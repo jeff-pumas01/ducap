6794781270c27d009e0a3ab4a23a21ad9724e705
Robert Fosen <RobertCFosen@lewisu.edu>
2017-10-13 12:10:07 -0500

    modified the volunteer application page

--- volunteerRegistration.php
+++ volunteerRegistration.php
@@ -25,12 +25,9 @@
 <div class = "container header">
 	<h2>  <b>Volunteer </b> Application </h2>
 	<br/>
-Click <a href="VolunteerForm.pdf">Here</a> to Download Volunteer Application </div>
-<!-- Start Form -->
-<Center>
-<iframe src='VolunteerApplication.php?sfm_from_iframe=1' frameborder='0' width='100%' height='1435' allowtransparency='true' align="middle"></iframe></Center>
-
-<!-- End Form -->
+Click <a href="VolunteerForm.pdf">Here</a> to Download Volunteer Application
+<br/>
+Click <a href="">Here</a> to apply online </div>
 
 
 <?php
